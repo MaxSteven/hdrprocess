785705f7d6ba336d7a59882d729bb9b006161a38
Colin Kennedy <colinvfx@gmail.com>
2016-03-08 09:48:43 -0500

    fixed sym-links

--- hdrprocess/controller/__init__.py
+++
@@ -1,1 +0,0 @@
-/home/selecaotwo/Dropbox/Private/my_PROJECT/proj_POP2/Pipeline/the_LATEST/sys_PY/py_MODULES/hdrprocess/controller/__init__.py
--- hdrprocess/model/plug-ins/hdrhtml/hdrhtml_hdrlabs_templ/hdrhtml_assets/mouse2touch.js
+++ hdrprocess/model/plug-ins/hdrhtml/hdrhtml_hdrlabs_templ/hdrhtml_assets/mouse2touch.js
@@ -1,1 +1,54 @@
-/home/selecaotwo/Dropbox/Private/my_PROJECT/proj_POP2/Pipeline/the_LATEST/sys_PY/py_MODULES/hdrprocess/model/plug-ins/hdrhtml/hdrhtml_hdrlabs_templ/hdrhtml_assets/mouse2touch.js
+/*
+---
+description: A MooTools plugin that automatically map mouse events to touch events
+
+license: MIT-style
+
+authors:
+- Chi Wai Lau (http://tabqwert.com)
+- Scott Kyle (http://appden.com)
+
+requires:
+- core/1.2.4: '*'
+
+provides: [Mouse2Touch]
+...
+*/
+
+if((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i)) || (navigator.userAgent.match(/iPad/i))) { 
+
+	(function() {
+	  try {
+	    document.createEvent("TouchEvent");
+	  } catch(e) {
+	    return;
+	  }
+	
+	  ['touchstart', 'touchmove', 'touchend'].each(function(type){
+	      Element.NativeEvents[type] = 2;
+	  });
+	
+	  var mapping = {
+	    'mousedown': 'touchstart',
+	    'mousemove': 'touchmove',
+	    'mouseup': 'touchend'
+	  };
+	
+	  var condition = function(event) {
+	    var touch = event.event.changedTouches[0];
+	    event.page = {
+	      x: touch.pageX,
+	      y: touch.pageY
+	    };
+	    return true;
+	  };
+	
+	  for (var e in mapping) {
+	    Element.Events[e] = {
+	      base: mapping[e],
+	      condition: condition
+	    };
+	  }
+	})();        
+
+}
